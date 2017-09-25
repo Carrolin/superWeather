fb0823545d180096eaa5c61222769128cdc57494
xrh <xrh2011start@163.com>
2017-09-25 08:09:22 +0800

    重新加入数据库和相关配置

---
+++ app/src/main/java/com/test/xrh/superweather/db/Province.java
@@ -0,0 +1,38 @@
+package com.test.xrh.superweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by admin on 2017/9/25.
+ */
+
+public class Province extends DataSupport{
+    //每个实体类都应该有的字段
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
